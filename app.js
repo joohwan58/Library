1e0cf640643a1de009fc4360b5b3f1e7a5b1ca23
Kim Joohwan <kim.joohwan58@gmail.com>
2023-06-13 18:05:08 +0900

    javascript for adding books mostly done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let Library = [];
+let library = document.querySelector('.content');
+console.log(library);
 
 function Book(title, author, hasBeenRead) {
     this.title = title;
@@ -6,3 +7,19 @@ function Book(title, author, hasBeenRead) {
     this.hasBeenRead = hasBeenRead;
 }
 
+Book.prototype.getElement = function () {
+    const book = document.createElement('div');
+    book.classList.add('book');
+    const title = document.createElement('h2');
+    title.textContent = this.title;
+    book.appendChild(title);
+    const author = document.createElement('p');
+    author.classList.add('author');
+    author.textContent = this.author;
+    book.appendChild(author);
+    // need to add checkmark and button
+    return book;
+}
+
+let test = new Book('naruto', 'kishimoto', true)
+library.appendChild(test.getElement()); 
