61aabe03f1c5401745e17c0ad22b7564f525186a
Kim Joohwan <kim.joohwan58@gmail.com>
2023-06-13 20:09:30 +0900

    javascript for creating book element from book class almost done

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 let library = document.querySelector('.content');
-console.log(library);
 
 function Book(title, author, hasBeenRead) {
     this.title = title;
@@ -18,8 +17,39 @@ Book.prototype.getElement = function () {
     author.textContent = this.author;
     book.appendChild(author);
     // need to add checkmark and button
+    const interaction = document.createElement('div');
+    interaction.classList.add('interaction');
+    const readForm = document.createElement('div');
+    readForm.classList.add('read-form');
+    const img = document.createElement('img');
+    img.classList.add('checkbox');
+    if (this.hasBeenRead) {
+        img.src = 'checkbox-check-svgrepo-com.svg';
+    } else {
+        img.src = 'checkbox-unchecked-svgrepo-com.svg';
+    }
+    //add checkbox event listener
+    readForm.appendChild(img);
+    const label = document.createElement('span');
+    label.classList.add('label');
+    label.textContent = 'Has been read';
+    readForm.appendChild(label);
+    interaction.appendChild(readForm);
+    const button = document.createElement('button');
+    button.classList.add('remove');
+    button.type = 'button'
+    button.textContent = 'Remove';
+    //add button event listener
+    interaction.appendChild(button);
+    book.appendChild(interaction);
     return book;
 }
 
-let test = new Book('naruto', 'kishimoto', true)
-library.appendChild(test.getElement()); 
+let newBook = new Book('naruto', 'kishimoto', true);
+console.log(library);
+library.appendChild(newBook.getElement())
+
+const test = document.querySelector('.book');
+test.children[2].children[0].children[0].addEventListener('click', function () {//long property chain calls img element
+    this.src = 'checkbox-check-svgrepo-com.svg';
+})
